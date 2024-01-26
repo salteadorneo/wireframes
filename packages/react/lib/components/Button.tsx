eeeb1e7c63ac95b96965531be590e5c0bed02665
Cristian Adán <hola@salteadorneo.dev>
2024-01-27 00:48:55 +0100

    Add props Button

--- packages/react/lib/components/Button.tsx
+++ packages/react/lib/components/Button.tsx
@@ -1,13 +1,26 @@
 import styles from '../styles.module.css'
 
 export function Button({
-  text = 'Button'
+  backgroundColor = 'white',
+  color = 'black',
+  borderColor = 'black',
+  children = 'Button',
 }: {
-  text?: string
+  backgroundColor?: string,
+  color?: string,
+  borderColor?: string,
+  children?: React.ReactNode
 }) {
   return (
-    <button className={`${styles.button}`}>
-      {text}
+    <button
+      className={`${styles.button}`}
+      style={{
+        backgroundColor,
+        color,
+        borderColor
+      }}
+    >
+      {children}
     </button>
   )
 }
