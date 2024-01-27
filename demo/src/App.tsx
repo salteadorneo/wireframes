931de3761498caa7d7a633f93ff0baf853f7faec
Cristian Adán <hola@salteadorneo.dev>
2024-01-27 01:17:43 +0100

    Add readme

---
+++ demo/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Avatar, Lorem } from 'wireframe-ui-react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Avatar />
+    </>
+  )
+}
+
+export default App
--- packages/react/src/App.tsx
+++ packages/react/src/App.tsx
@@ -5,29 +5,66 @@ import { Image } from "../lib/components/Image";
 import { Lorem } from "../lib/components/Lorem";
 import { H1, H2, H3, H4, H5, H6 } from "../lib/components/Heading";
 import { P } from "../lib/components/Paragraph";
-import { Flex } from "../lib/components/Display";
+import { Flex, Grid } from "../lib/components/Display";
 import { Video } from "../lib/components/Video";
 
 export default function App() {
     return (
         <>
-            <Flex>
-                <Avatar />
-                <P>User</P>
-            </Flex>
-            <H1><Lorem /></H1>
-            <H2><Lorem /></H2>
-            <H3><Lorem /></H3>
-            <H4><Lorem /></H4>
-            <H5><Lorem /></H5>
-            <H6><Lorem /></H6>
-            <P><Lorem /></P>
-            <Link />
-            <div>
-                <Button />
-            </div>
-            <Image width={200} />
-            <Video width={600} />
+            <header>
+                <Flex justifyContent="space-between" alignItems="center" gap={10}>
+                    <Image width={150} height={60} />
+                    <Flex alignItems="center" gap={10}>
+                        <P>User</P>
+                        <Avatar size={32} />
+                    </Flex>
+                </Flex>
+            </header>
+            <main>
+                <H1><Lorem /></H1>
+                <H2><Lorem /></H2>
+                <H3><Lorem /></H3>
+                <H4><Lorem /></H4>
+                <H5><Lorem /></H5>
+                <H6><Lorem /></H6>
+                <P><Lorem /></P>
+                <Link />
+                <Flex gap={8}>
+                    <Button />
+                    <Button
+                        backgroundColor="black"
+                        color="white"
+                    />
+                </Flex>
+                <Flex
+                    gap={4}
+                    justifyContent="space-around"
+                >
+                    <Grid justifyItems="center" gap={12}>
+                        <Image width={"100%"} aspectRatio={16 / 9} />
+                        <P textAlign="center"><Lorem /></P>
+                        <Button />
+                    </Grid>
+                    <Grid justifyItems="center" gap={12}>
+                        <Image width={"100%"} aspectRatio={16 / 9} />
+                        <P textAlign="center"><Lorem /></P>
+                        <Button />
+                    </Grid>
+                    <Grid justifyItems="center" gap={12}>
+                        <Image width={"100%"} aspectRatio={16 / 9} />
+                        <P textAlign="center"><Lorem /></P>
+                        <Button />
+                    </Grid>
+                </Flex>
+                <Video />
+            </main>
+            <footer>
+                <Flex gap={8}>
+                    <Link />
+                    <Link />
+                    <Link />
+                </Flex>
+            </footer>
         </>
     );
 }
